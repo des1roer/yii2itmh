74764f08f5acfc0c0e688850ff7e6525cc242fc5
zuev_np <zuev_np@ID003576.vgok.ru>
2016-03-23 10:34:13 +0500

    8 добавлен codeception

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=itmh',
+    'dsn' => 'mysql:host=localhost;dbname=itmh', //prod - itmh, test - itmh_tests
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
